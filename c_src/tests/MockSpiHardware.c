58d139ce29dc208ce62903c2d27fbace3766012b
Darth Rubik <gamblejr@prototypeind.com>
2018-02-17 21:44:02 -0600

    Adding unit tests for the spi controller
    Adding unit tests for the new spi controller

--- c_src/tests/MockSpiHardware.c
+++ c_src/tests/MockSpiHardware.c
@@ -1,8 +1,22 @@
 #include "CppUTestExt/MockSupport_c.h"
 #include "SpiHardware.h"
 
+void IntializeSpi(SpiPackage_t* spi, SpiHw_t* hws, Pio_t* Active, SpiId_t count)
+{
+}
 void StartSpiMessage(SpiPackage_t* spi, SpiId_t id){
 	mock_c()->actualCall("StartSpiMessage")->
 		withPointerParameters("spi",spi)->
 		withIntParameters("id",id);
+}
+void WriteWordToSpi(SpiPackage_t* spi, uint32_t word)
+{
+	mock_c()->actualCall("WriteWordToSpi")->
+		withPointerParameters("spi",spi)->
+		withIntParameters("word",word);
+}
+void EndSpiMessage(SpiPackage_t* spi)
+{
+	mock_c()->actualCall("EndSpiMessage")->
+		withPointerParameters("spi",spi);
 }
--- c_src/tests/SpiControllerTests.cpp
+++ c_src/tests/SpiControllerTests.cpp
@@ -8,29 +8,138 @@ TEST_GROUP(SpiController)
 {
 
 	SpiController_t spi;
+	SpiPackage_t package;
 	SpiMessage_t message;
+	static const size_t bufferSize = 10;
+	void* buffer[bufferSize];
 	void setup(){
+		InitializeSpiController(&spi,&package,buffer,bufferSize);
+		mock().strictOrder();
 	}
-	void teardown() {}
+	void teardown() {
+		mock().clear();
+	}
 };
 
 
-IGNORE_TEST(SpiController, VerifySender)
+TEST(SpiController, VerifyShortMessage)
 {
+	mock().expectOneCall("StartSpiMessage").withParameter("spi",&package).withParameter("id",0);
+	mock().expectOneCall("WriteWordToSpi").withParameter("spi",&package).withParameter("word",42);
+	mock().expectOneCall("EndSpiMessage").withParameter("spi",&package);
+
+	uint32_t data[] = { 42 };
+	message.data = data;
+	message.messageLength = 1;
+	message.id = 0;
+	QueueSpiMessage(&spi, &message);
+
+
+	SpiChunkSet(&spi);
+	mock().checkExpectations();
+
+}
+TEST(SpiController, VerifyLongMessage)
+{
+	mock().expectOneCall("StartSpiMessage").withParameter("spi",&package).withParameter("id",0);
+	for (int x = 0; x < 10; x++)
+		mock().expectOneCall("WriteWordToSpi").withParameter("spi",&package).withParameter("word",x+22);
+
+	mock().expectOneCall("EndSpiMessage").withParameter("spi",&package);
+
 		
+	uint32_t data[] = { 22,23,24,25,26,27,28,29,30,31};
+	message.data = data;
+	message.messageLength = 10;
+	message.id = 0;
+
+	QueueSpiMessage(&spi,&message);
+
+	for (int x = 0; x < 10; x++)
+	{
+		SpiChunkSet(&spi);
+	}
+	mock().checkExpectations();
 }
-IGNORE_TEST(SpiController, VerifyShortMessage)
+TEST(SpiController, QueueMultipleMessages)
 {
+	uint32_t data[] = { 0x10,0x20,0x30 };
+	SpiMessage_t messages[3];
+	for (int x = 0; x < 3; x++)
+	{
+		messages[x].data = &data[x];
+		messages[x].messageLength = 1;
+		messages[x].id = x;
+		mock().expectOneCall("StartSpiMessage").withParameter("spi",&package).withParameter("id",x);
+		mock().expectOneCall("WriteWordToSpi").withParameter("spi",&package).withParameter("word",data[x]);
+		mock().expectOneCall("EndSpiMessage").withParameter("spi",&package);
+	}
+	QueueSpiMessage(&spi,&messages[0]);
+	QueueSpiMessage(&spi,&messages[1]);
+	QueueSpiMessage(&spi,&messages[2]);
+
+	for (int x = 0; x < 3; x++)
+	{
+		SpiChunkSet(&spi);
+	}
+	mock().checkExpectations();
 }
-IGNORE_TEST(SpiController, VerifyLongMessage)
+TEST(SpiController, QueueMaxMessages)
 {
+	uint32_t data[bufferSize+1];
+	SpiMessage_t messages[bufferSize+1];
+	for (int x = 0; x < bufferSize+1; x++)
+	{
+		data[x] = x;
+		messages[x].data = &data[x];
+		messages[x].messageLength = 1;
+		messages[x].id = x;
+		mock().expectOneCall("StartSpiMessage").withParameter("spi",&package).withParameter("id",x);
+		mock().expectOneCall("WriteWordToSpi").withParameter("spi",&package).withParameter("word",data[x]);
+		mock().expectOneCall("EndSpiMessage").withParameter("spi",&package);
+	}
+	for (int x = 0; x < bufferSize+1; x++)
+	{
+		QueueSpiMessage(&spi,&messages[x]);
+	}
+	for (int x = 0; x < bufferSize+1; x++)
+	{
+		SpiChunkSet(&spi);
+	}
+	mock().checkExpectations();
 }
-IGNORE_TEST(SpiController, QueueMultipleMessages)
+TEST(SpiController, OverflowQueue)
 {
+
+	uint32_t data[bufferSize+2];
+	SpiMessage_t messages[bufferSize+2];
+	
+
+	for (int x = 0; x < bufferSize+2; x++)
+	{
+		data[x] = x;
+		messages[x].data = &data[x];
+		messages[x].messageLength = 1;
+		messages[x].id = x;
+		if (x != bufferSize + 1)
+		{
+			mock().expectOneCall("StartSpiMessage").withParameter("spi",&package).withParameter("id",x);
+			mock().expectOneCall("WriteWordToSpi").withParameter("spi",&package).withParameter("word",data[x]);
+			if (x == 0) mock().expectOneCall("DeclareError");
+			mock().expectOneCall("EndSpiMessage").withParameter("spi",&package);
+		}
+	}
+	for (int x = 0; x < bufferSize+2; x++)
+	{
+		QueueSpiMessage(&spi,&messages[x]);
+	}
+	for (int x = 0; x < bufferSize+1; x++)
+	{
+		SpiChunkSet(&spi);
+	}
+	mock().checkExpectations();
 }
-IGNORE_TEST(SpiController, QueueMaxMessages)
+IGNORE_TEST(SpiController, SendTimeout)
 {
-}
-IGNORE_TEST(SpiController, RunWithEmptyMessageQueue)
-{
+	
 }
