6376abbd36fc62d1768ba401bce4ca998cbdfc4b
Darth Rubik <gamblejr@prototypeind.com>
2018-03-06 06:45:51 -0600

    Updating the project to include the actual code
    Now the project has some code that actually runs on the target board.......we have tested and the we can talk to the ADC chip, which is good

--- c_src/main.c
+++ c_src/main.c
@@ -1,52 +1,87 @@
-#include "stdint.h"
-typedef struct{
-	volatile uint32_t rxdata;
-	volatile uint32_t txdata;
-	volatile uint32_t status;
-	volatile uint32_t control;
-	volatile uint32_t reserved;
-	volatile uint32_t slaveselect;
-	volatile uint32_t eop_value;
-} __attribute__((packed)) SpiHw_t;
+#include "src/SpiController.h"
+#include "src/Adc.h"
+#include "stdio.h"
 
-typedef struct
+#define CAN_SPI_PORT (((SpiHw_t*)0xff200220))
+#define ADC_SPI_PORT (((SpiHw_t*)0xff200240))
+#define SPI_SELECT      ((Pio_t*)0xff200200)
+
+#define LS_SW_PORT (((Pio_t*)0xff200120))
+
+#ifdef USE_INTERRUPTS
+#define INTERRUPT __attribute__((interrupt))
+#else
+#define INTERRUPT
+#endif
+
+static SpiTarget_t spiTargets[] =
 {
-	volatile uint32_t data;
-	volatile uint32_t direction;
-	volatile uint32_t interruptMask;
-	volatile uint32_t edgeCapture;
-} __attribute__((packed)) Pio_t;
-
-#define CAN_SPI_PORT (*((SpiHw_t*)0xff200220))
-#define LS_SW_PORT (((Pio_t*)0xff200120)->data)
-#define DO_SPI_TEST
-
-
-
+	{
+		CAN_SPI_PORT,
+		0,
+		0
+	},
+	{
+		CAN_SPI_PORT,
+		0,
+		1,
+	},
+	{
+		CAN_SPI_PORT,
+		0,
+		2,
+	},
+	{
+		ADC_SPI_PORT,
+		1,
+		0,
+	},
+	{
+		ADC_SPI_PORT,
+		1,
+		1,
+	},
+};
 
 int main(void)
 {
-
-	
-	#ifdef DO_SPI_TEST
-	while (1)
+	/*
+	CAN_SPI_PORT->txdata = 0xaa;
+	while(1)
 	{
-		CAN_SPI_PORT.txdata = 0xAA;
-		
-		while (!(CAN_SPI_PORT.status & (1<<6)));
-	}
-	#else
-	while (1)
-	{
-		LS_SW_PORT ^= 1;
-		
-		volatile uint64_t x = 0;
-		for (x = 0; x < 4000000; x++)
+		if (CAN_SPI_PORT->status & (1<<7))
 		{
-			
+			CAN_SPI_PORT->txdata = 0xaa;
 		}
-		
-		
+	}*/
+	
+	
+	SpiPackage_t spipackage;
+	SpiController_t spi;
+	void* spiBuffer[10];
+	InitializeSpi(&spipackage,spiTargets,SPI_SELECT,
+			sizeof(spiTargets)/sizeof(spiTargets[0]));
+	InitializeSpiController(&spi,&spipackage,spiBuffer,10);
+	Adc_t adc;
+	InitializeAdc(&adc,&spi,3,1<<1);
+
+	while (1){
+
+		UpdateAdc(&adc);
+		printf("Reading: %i\n", GetAdcReading(&adc,1));
+		if (ADC_SPI_PORT->status & (1<<7))
+		{
+			ADC_SPI_PORT->status &= ~(1<<7);
+			uint64_t data = ADC_SPI_PORT->rxdata;
+			SpiChunkSet(&spi,data);	
+		}
 	}
-	#endif
+
+}
+
+void INTERRUPT __cs3_isr_dabort(void)
+{
+	int x = 1;
+	while (x){}
+	return;
 }
